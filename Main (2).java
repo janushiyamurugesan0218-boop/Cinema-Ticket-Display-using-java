379b6527feb7c2883e52a9ad92b9b11ff98c04b1
Janushiya <janushiyamurugesan0218@gmail.com>
2026-07-03 21:16:52 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,13 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String name="Karupu";
+	    int screenNumber=12;
+	    String seatNumber="A12";
+		System.out.println("Movie Ticket="+name);
+		System.out.println(screenNumber);
+		System.out.println(seatNumber);
+	}
+}
